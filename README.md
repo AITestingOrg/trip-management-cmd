2d3ce455818f222fcc001f0b8e5a950dd9955ad4
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-02-23 15:55:14 -0500

    Configuration and Readme update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # trip-management-cmd
+
+The command side of the trip management microservices.
+
+To run this project from docker:
+* Make sure that the Docker daemon is installed and running.
+* Assemble this project.
+* Run ```docker-compose up --build``` from the root project.
+* On another terminal run ```docker ps``` to see which ports were assigned.
+ 
--- src/main/java/org/aitesting/microservices/tripmanagement/cmd/service/configuration/TripAmqpEventConfiguration.java
+++
@@ -1,8 +0,0 @@
-package org.aitesting.microservices.tripmanagement.cmd.service.configuration;
-
-import org.aist.libs.eventsourcing.configuration.configurations.AmqpEventSubscriptionConfiguration;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TripAmqpEventConfiguration extends AmqpEventSubscriptionConfiguration{
-}
